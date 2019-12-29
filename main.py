ba969287e9dd0fec6a60d2501d936e95c8273e9a
Marco Bartsch <contact@marcobartsch.me>
2019-12-29 07:07:28 +0100

    add up and down transition

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ LEDsRight = NeoPixel(Pin(18, Pin.OUT), stripSize, bpp=3)
 LEDsLeft = NeoPixel(Pin(17, Pin.OUT), stripSize, bpp=3)
 
 
-def rainbow(t, i):
+def rainbow(t, i, brightness):
     a = (0.5, 0.5, 0.5)
     b = (0.5, 0.5, 0.5)
     c = (1.0, 1.0, 1.0)
@@ -21,17 +21,34 @@ def rainbow(t, i):
     g = a[1] + b[1] * math.cos(6.28318 * (c[1] * k + d[1]))
     b = a[2] + b[2] * math.cos(6.28318 * (c[2] * k + d[2]))
 
-    return (int(255.0 * r), int(255.0 * g), int(255.0 * b))
+    return (int(255.0 * r * brightness), int(255.0 * g * brightness), int(255.0 * b * brightness))
 
 
-def render():
+def change_brightness(brightness, direction):
+    if direction == 'up':
+        brightness = brightness + 0.05
+    else:
+        brightness = brightness - 0.05
+
+    if brightness <= 0.1:
+        direction = 'up'
+    elif brightness >= 1:
+        direction = 'down'
+
+    return brightness, direction
+
+
+def rainbow_animation(brightness=0.3, up_and_down=False):
 
     t = 0.0
+
+    if up_and_down:
+        brightness, direction = change_brightness(0.1, 'up')
 
     while True:
         t += 0.06
         for i in range(stripSize):
-            color = rainbow(t, i)
+            color = rainbow(t, i, brightness)
             LEDsBottom[i] = color
             LEDsRight[i] = color
             LEDsLeft[i] = color
@@ -40,7 +57,10 @@ def render():
         LEDsRight.write()
         LEDsLeft.write()
 
+        if up_and_down:
+            brightness, direction = change_brightness(brightness, direction)
+
         time.sleep(1.0/36.0)
 
 
-render()
+rainbow_animation()
